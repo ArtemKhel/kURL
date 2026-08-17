c71ea23bacf0c49f3007220e91df1e14f76523bb
ArtemKhel <ArtemKhel@gmail.com>
2026-08-17 10:27:30 +0300

    chore: event script

--- scripts/mk_event.fish
+++ scripts/mk_event.fish
@@ -1,5 +1,8 @@
 #!/usr/bin/env fish
 
 for i in (seq 1 $argv[1])
-    redis-cli xadd Events '*' event "{\"short_code\":\"$(xxd -l 1 -p /dev/urandom | head -c 1)\",\"at\":\"$(date -Iseconds)\"}" > /dev/null &
+    set code (xxd -l 1 -p /dev/urandom | head -c 1)
+    set at (date +'%Y-%m-%dT%H:%M:%S.%N%:z')
+    set event (jq -nc --arg code "$code" --arg at "$at" '{"short_code": $code, "at": $at}')
+    redis-cli xadd Events '*' event "$event" > /dev/null &
 end
