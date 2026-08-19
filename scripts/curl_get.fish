3a254fd9f62f0a8a2a493a813885205379924542
ArtemKhel <ArtemKhel@gmail.com>
2026-08-19 07:51:45 +0300

    test(curl_get): --silent

--- scripts/curl_get.fish
+++ scripts/curl_get.fish
@@ -2,5 +2,5 @@
 
 for i in (seq 1 $argv[1])
     set code (xxd -l 1 -p /dev/urandom | head -c 1)
-    curl -X GET "http://localhost:3000/s/$code" > /dev/null &
+    curl --silent -X GET "http://localhost:3000/s/$code" > /dev/null &
 end
